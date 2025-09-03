d0cde0058011c25311b2356db8ce914ab6c4dd16
ANURAG SWAIN <anuraaghthegreat@gmail.com>
2025-09-04 02:20:42 +0530

    Python Programming

--- 17.MEGA_PROJECT_JARVIS/musicLibrary.py
+++
@@ -1,7 +0,0 @@
-music = {
-    "don" : "https://youtu.be/qJL648b-qV0?si=ZCss-jwmpBlG9GR0",
-    "ton" : "https://youtu.be/M36Q_Phm8fg?si=YS_Yc2E7MNov5SHc",
-    "moon" : "https://youtu.be/Umqb9KENgmk?si=KdaYH7kLCPsQPRe1",
-    "skyfall" : "https://youtu.be/DeumyOzKqgI?si=jhiLRzeijO12lpwo"
-
-}
--- 18.MEGA_PROJECT_AUTO-REPLY-AI-CHATBOT/1_get_cursor.py
+++
@@ -1,9 +0,0 @@
-# AUTO REPLY BOT
-
-import pyautogui
-
-while True:
-    a = pyautogui.position()
-    print(a)
-    # 598 , 1046
-    # 656 , 383 to 1198 , 939
